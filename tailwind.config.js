1d96f8b0ad3e7092c979d6d0b6b1049ecee8da1a
Nuno Mendes <nunomendesx40@gmail.com>
2026-04-29 09:58:12 +0100

    update2

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./portfolio.jsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
